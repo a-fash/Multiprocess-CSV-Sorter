380502050785b3533350c762cca5ac870e72376f
a-fash <andrew.fash@rutgers.edu>
2022-10-10 20:57:39 -0400

    Added updated scannerCSVsorter.c file

--- scannerCSVsorter.c
+++ scannerCSVsorter.c
@@ -1,100 +1,194 @@
-#include "scannerCSVsorter.h"
+#include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <limits.h>
+#include <time.h>
+#include "directoryAndFile.h"
 
-char *stringTitles[] = {"color", "director_name", "actor_2_name", "genres", "actor_1_name", "movie_title", "actor_3_name", "plot_keywords", "movie_imdb_link", "language", "country", "content_rating"};
+char *sortColumn;
+char *sortDirectory;
+char *outputDirectory;
+int hasSortColumn;
+int hasSortDirectory;
+int hasOutputDirectory;
 
-char *numericTitles[] = {"num_critic_for_reviews", "duration", "director_facebook_likes", "actor_3_facebook_likes", "actor_1_facebook_likes", "gross", "num_voted_users", "cast_total_facebook_likes", "facenumber_in_poster", "num_user_for_reviews", "budget", "title_year", "actor_2_facebook_likes", "imdb_score", "aspect_ratio", "movie_facebook_likes"};
+void freeData();
+int procCount(char *);
+
+int sortColumnType(char *);
+
+int NULLTERMINATOR = 1;
+
+int main(int argc, char *argv[])
+{
+	clock_t start, end;
+	double diff;
+
+	start = clock();
 
 
+	int maxFileAndDirectories = 255;
+	int maxPIDlength = 5;
 
-
-int main(int argc, char *argv[]){
-
-	char *sortColumn;
-        char *sortDirectory;
-        char *outputDirectory;
-        int hasSortColumn = 0;
-        int hasSortDirectory = 0;
-        int hasOutputDirectory = 0;
+	hasSortColumn = 0;
+	hasSortDirectory = 0;
+	hasOutputDirectory = 0;
 	char *flag;
-	char *field;
 
-        if(argc-1 > 6)
-        {
-                fprintf(stderr, "ERROR: Input fields is greater than 6");
-                return -1;
-        }
+	if(argc-1 > 6)
+	{
+		fprintf(stderr, "ERROR: Input fields is greater than 6\n");
+		freeData();
+		return -1;
+	}
 
 	if((argc-1) % 2 == 1)
-        {
-                fprintf(stderr, "ERROR: Flag missing field or field missing flag");
-                return -1;
-        }
+	{
+		fprintf(stderr, "ERROR: Flag missing field or field missing flag\n");
+		freeData();
+		return -1;
+	}
 
 	int i;
 	for(i=1; i < argc; i+=2)
-        {
-
-		field = (char *)malloc(sizeof(char)*strlen(argv[i]));
-                strcat(field, argv[i]);
-		if(strcmp("-c", field) == 0)
-                {
-			flag = (char *)malloc(sizeof(char)*sizeof(argv[i+1]));
-			strcpy(flag, argv[i+1]);
-			sortColumn = (char *)malloc(sizeof(char)*sizeof(argv[i+1]));
-			strcpy(sortColumn, flag);
-	                hasSortColumn =	1;
-                }else if(strcmp("-d", field) == 0)
-	                {
-			flag = (char *)malloc(sizeof(char)*sizeof(argv[i+1]));
-			strcpy(flag, argv[i+1]);
-			sortDirectory = (char *)malloc(sizeof(char)*sizeof(field));
-			strcpy(sortDirectory, flag);
-                        hasSortDirectory = 1;
-                }else if(strcmp("-o", field) == 0)
-                {
-			flag = (char *)malloc(sizeof(char)*sizeof(argv[i+1]));
-			strcpy(flag, argv[i+1]);
-			outputDirectory = (char *)malloc(sizeof(char)*sizeof(field));
-			strcpy(outputDirectory, flag);
-                        hasOutputDirectory = 0;
-                } else {
-                        fprintf(stderr, "ERROR: INCORRECT FLAG \"&s\" \n", argv[i+1]);
-                        return -1;
-                }
-        }
-
-        if(hasSortColumn == 0)
-        {
-                fprintf(stderr, "FATAL ERROR: No sort column given");
-                return -1;
-        }
-	if(checkColumn(sortColumn) < 0)
 	{
-		fprintf(stderr, "FATAL ERROR: Sort column incorrect");
+		flag = (char *)malloc(sizeof(char) * (strlen(argv[i]) + NULLTERMINATOR) );
+		strcpy(flag, argv[i]);
+
+		if(strcmp("-c", flag) == 0)
+		{
+			sortColumn = (char *)malloc(sizeof(char) * (strlen(argv[i+1]) + NULLTERMINATOR) );
+			strcpy(sortColumn, argv[i+1]);
+			hasSortColumn =	1;
+		}else if(strcmp("-d", flag) == 0)
+		{
+			sortDirectory = (char *)malloc(sizeof(char) * (strlen(argv[i+1]) + NULLTERMINATOR) );
+			strcpy(sortDirectory, argv[i+1]);
+			hasSortDirectory = 1;
+		}else if(strcmp("-o", flag) == 0)
+		{
+			outputDirectory = (char *)malloc(sizeof(char) * (strlen(argv[i+1]) + NULLTERMINATOR) );
+			strcpy(outputDirectory, argv[i+1]);
+			hasOutputDirectory = 1;
+		} else
+		{
+			fprintf(stderr, "ERROR: INCORRECT FLAG %s \n", argv[i+1]);
+			free(flag);
+			freeData();
+			return -1;
+		}
+		free(flag);
+	}
+
+	if(hasSortColumn == 0)
+	{
+		fprintf(stderr, "FATAL ERROR: No sort column given\n");
+		freeData();
 		return -1;
 	}
-	 if(hasSortDirectory == 0)
-        {
-                sortDirectory = "./";
-        }
+	if(sortColumnType(sortColumn) < 0)
+	{
+		fprintf(stderr, "FATAL ERROR: Sort column incorrect\n");
+		freeData();
+		return -1;
+	}
 
+	char cwd[PATH_MAX + NULLTERMINATOR];
+
+	if(hasSortDirectory == 0)
+        {
+		if(getcwd(cwd, sizeof(cwd)) != NULL)
+		{
+			sortDirectory = (char *)malloc(sizeof(char) * (strlen(cwd) + NULLTERMINATOR) );
+			strcpy(sortDirectory, cwd);
+			printf("sort directory: %s\n", sortDirectory);
+		} else
+		{
+			fprintf(stderr, "Fatal ERROR: cannot get current working directory for sort directory\n");
+			freeData();
+			return 1;
+		}
+        }
         if(hasOutputDirectory == 0)
         {
-                outputDirectory = "./";
+		if(getcwd(cwd, sizeof(cwd)) != NULL)
+		{
+			outputDirectory = (char *)malloc(sizeof(char) * (strlen(cwd) + NULLTERMINATOR) );
+			strcpy(outputDirectory, cwd);
+		} else
+		{
+			fprintf(stderr, "Fatal ERROR: cannot get current working directory for output directory\n");
+			freeData();
+			return 1;
+		}
         }
 
-        fprintf(stdout, "Initial PID: %d\n", getpid());
-        fprintf(stdout, "PIDS of all child processes: ");
+	fprintf(stdout, "Initial PID: %d\n", getpid());
 
-        int numProcs = directoryHandler(sortDirectory, outputDirectory, sortColumn);
+	FILE *temp = tmpfile();
 
-        fprintf(stdout, "\n");
-        fprintf(stdout,	"Total number of processes: %d\n", numProcs);
+	fprintf(temp, "PIDS of all child processes: ");
 
-	free(field);
-	free(flag);
-	free(sortColumn);
-	free(sortDirectory);
-	free(outputDirectory);
-        return 0;
+	//Use stdout as buffer for child pids
+	//fprintf(stdout, "PIDS of all child processes: ");
+	//fflush(stdout);
+
+	fflush(temp);
+
+        directoryHandler(sortDirectory, outputDirectory, sortColumn, temp);
+
+	rewind(temp);
+
+	char *childProcs = (char *)malloc(sizeof(char) * (maxFileAndDirectories * (maxPIDlength + 1)) );
+
+	fgets(childProcs, maxFileAndDirectories * (maxPIDlength + 1), temp);
+
+	fclose(temp);
+
+	fprintf(stdout, "%s\n", childProcs);
+
+	//For stdout buffer
+	//fprintf(stdout, "\n");
+
+        fprintf(stdout,	"Total number of processes: %d\n", procCount(childProcs));
+
+	end = clock();
+	diff = (double)(end - start) / CLOCKS_PER_SEC;
+
+	fprintf(stdout,	"Time taken %f\n", diff);
+
+	free(childProcs);
+	freeData();
+
+	return 0;
+}
+
+void freeData() {
+
+	if(hasSortColumn == 1)
+		free(sortColumn);
+	if(hasSortDirectory == 1)
+		free(sortDirectory);
+	if(hasOutputDirectory == 1)
+		free(outputDirectory);
+
+	return;
+}
+
+int procCount(char *CSpid)
+{
+	char *ptr;
+	int count = 0;
+
+	ptr = CSpid + strlen("PIDS of all child processess: ");
+
+	while (*ptr != '\0')
+	{
+		ptr = strchr(ptr, ',');
+		count++;
+		ptr++;
+	}
+
+	return count;
 }
